4cf4d7a1ca3af16633c8c09f2b3b1a79f7fdaea5
deepakmizzle <deepak.s@mizzle.io>
2024-09-07 09:29:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to Data.
+          Edit <code>src/App.js</code> and Extract and save According to us .
         </p>
         <a
           className="App-link"
